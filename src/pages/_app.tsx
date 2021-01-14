ef23a2bb51c0f6491b6c6128809fd8903f8c86cf
NakSeoNo <nakseono@gmail.com>
2021-01-14 11:54:46 +0900

    add tailwind css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
+import 'tailwindcss/tailwind.css'
 
 export default ({ Component, pageProps }) => (
   <>
