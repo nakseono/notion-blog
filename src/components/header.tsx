f8f48a8a25a2ef282494e8d04590323b5b507a1a
NakSeoNo <nakseono@gmail.com>
2021-01-25 18:01:19 +0900

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,9 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  // { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  { label: 'About Me', page: '/' },
+  { label: 'Posts', page: '/blog' },
+  { label: 'Timeline', page: '/timeline' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -19,17 +18,9 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
-        <meta
-          name="description"
-          content="An example Next.js site using Notion for the blog"
-        />
-        <meta name="og:title" content="My Notion Blog" />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
+        <title>{titlePre ? `${titlePre} |` : ''} NakSeo's Tech Blog</title>
       </Head>
+      <div>This is Avator Space</div>
       <ul>
         {navItems.map(({ label, page, link }) => (
           <li key={label}>
