4b94ecbf17672d21ba9b1510cd640e8946778808
NakSeoNo <nakseono@gmail.com>
2021-01-28 16:07:55 +0900

    delete dud darkmode btn

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import ExtLink from './ext-link'
 import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
-import { useTheme } from 'next-themes'
-import { Switch } from 'antd'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'About Me', page: '/' },
@@ -15,27 +13,12 @@ const navItems: { label: string; page?: string; link?: string }[] = [
 
 export default ({ titlePre = '' }) => {
   const { pathname } = useRouter()
-  const [isMounted, setIsMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-  const switchTheme = () => {
-    if (isMounted) {
-      setTheme(theme === 'light' ? 'dark' : 'light')
-    }
-    if (theme === 'dark') {
-    }
-  }
 
   return (
     <header className={styles.header}>
       <Head>
         <title>{titlePre ? `${titlePre} |` : ''} NakSeo's Tech Blog</title>
       </Head>
-      <button className="absolute right-7" onClick={switchTheme}>
-        다크모드
-      </button>
       <div>
         <div className="left-1/2">This is Avator Space</div>
       </div>
@@ -58,7 +41,6 @@ export default ({ titlePre = '' }) => {
           </li>
         ))}
       </ul>
-      <Switch className="w-5 h-5" onClick={switchTheme} />
     </header>
   )
 }
