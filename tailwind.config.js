5eed0050deff94a1e46949d15da6c52026960561
nakseono <nakseono@gmail.com>
2021-02-03 17:30:54 +0900

    set color palette, h1 weight

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    backgroundColor: {
+      bright: '#FFF8E1',
+      dark: '#616161',
+      gray: '#BDBDBD',
+      beige: '#D7CCC8',
+      orange: '#FFB74D',
+    },
+    textColor: {
+      bright: '#FFF8E1',
+      dark: '#616161',
+      gray: '#BDBDBD',
+      beige: '#D7CCC8',
+      orange: '#FFB74D',
+    },
     colors: {
       gray: colors.trueGray,
     },
