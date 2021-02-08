f6df503b9205e9dde31b65fbeca007a034dd89ca
nakseono <nakseono@gmail.com>
2021-02-08 11:21:53 +0900

    블로그 색상 팔레트 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,16 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     backgroundColor: {
-      bright: '#FFF8E1',
-      dark: '#616161',
-      gray: '#BDBDBD',
-      beige: '#D7CCC8',
-      orange: '#FFB74D',
+      dark1: '#4d4646',
+      dark2: '#5b5656',
+      gr: '#7fcd91',
+      bright1: '#f5eaea',
     },
     textColor: {
-      bright: '#FFF8E1',
-      dark: '#616161',
-      gray: '#BDBDBD',
-      beige: '#D7CCC8',
-      orange: '#FFB74D',
+      dark1: '#4d4646',
+      dark2: '#5b5656',
+      gr: '#7fcd91',
+      bright1: '#f5eaea',
     },
     colors: {
       gray: colors.trueGray,
